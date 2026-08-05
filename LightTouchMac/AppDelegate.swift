c213900111b46f4f50d74ae306a47a64a18d12ac
samhenrigold <49251320+samhenrigold@users.noreply.github.com>
2026-08-05 12:34:36 -0400

    Initial Commit

---
+++ LightTouchMac/AppDelegate.swift
@@ -0,0 +1,25 @@
+// Created by Sam on 2026-08-05.
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
